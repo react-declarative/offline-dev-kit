d17288ad1842e09766bdb2d3bd21ca9b5fe186d3
Petr Tripolsky <tripolskypetr@gmail.com>
2022-03-07 20:21:37 +0300

    esm-wrapper

--- packages/mui-grid-umd/src/alias/Autocomplete.js
+++ packages/mui-grid-umd/src/alias/Autocomplete.js
@@ -1,1 +1,41 @@
-module.exports = MuiMaterial.Autocomplete;
+const Autocomplete = (props) => React.createElement(MuiMaterial.Autocomplete, props);
+Autocomplete.createFilterOptions = function createFilterOptions(config = {}) {
+    const {
+        ignoreAccents = true,
+        ignoreCase = true,
+        limit,
+        matchFrom = 'any',
+        stringify,
+        trim = false
+    } = config;
+    return (options, {
+        inputValue,
+        getOptionLabel
+    }) => {
+        let input = trim ? inputValue.trim() : inputValue;
+
+        if (ignoreCase) {
+            input = input.toLowerCase();
+        }
+
+        if (ignoreAccents) {
+            input = stripDiacritics(input);
+        }
+
+        const filteredOptions = options.filter(option => {
+            let candidate = (stringify || getOptionLabel)(option);
+
+            if (ignoreCase) {
+                candidate = candidate.toLowerCase();
+            }
+
+            if (ignoreAccents) {
+                candidate = stripDiacritics(candidate);
+            }
+
+            return matchFrom === 'start' ? candidate.indexOf(input) === 0 : candidate.indexOf(input) > -1;
+        });
+        return typeof limit === 'number' ? filteredOptions.slice(0, limit) : filteredOptions;
+    };
+};
+module.exports = Autocomplete;
